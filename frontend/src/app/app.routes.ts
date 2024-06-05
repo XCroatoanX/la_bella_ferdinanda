484ebdda7d57e41879abf563564c5cbfaea35096
Artem Stasyuk <stasyukartem2@gmail.com>
2024-06-06 00:19:45 +0200

    Small updates

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { authGuard } from './auth/auth.guard';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'auth/login', component: LoginComponent},
+    {path: 'admin', component: AdminPanelComponent, canActivate: [authGuard]}
+];
