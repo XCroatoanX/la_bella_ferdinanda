d889498c34fd0ea8e8af48884afe545d9521c18a
XCroatoanX <stasyukartem2@gmail.com>
2024-06-06 15:34:04 +0200

    Added Login page

--- frontend/src/app/admin-panel/admin-panel.component.ts
+++ frontend/src/app/admin-panel/admin-panel.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-panel',
   standalone: true,
   imports: [],
   templateUrl: './admin-panel.component.html',
-  styleUrl: './admin-panel.component.scss'
+  styleUrl: './admin-panel.component.scss',
 })
 export class AdminPanelComponent {
+  constructor(private authService: AuthService, private router: Router) {}
 
+  public onLogout(): void {
+    this.authService.logOut();
+    this.router.navigate(['/auth/login']);
+  }
 }
