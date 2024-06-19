7c541cbf346a4e20e7176207704d848c1ef6a4c6
XCroatoanX <stasyukartem2@gmail.com>
2024-06-19 16:49:52 +0200

    Various changes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,9 +4,31 @@ import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 import { authGuard } from './auth/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { CreateCatComponent } from './admin-panel/create-cat/create-cat.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutUsComponent } from './about/about-us/about-us.component';
+import { AboutBreedComponent } from './about/about-breed/about-breed.component';
+import { HowToCareComponent } from './about/how-to-care/how-to-care.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  {
+    path: 'about',
+    children: [
+      {
+        path: 'about-us',
+        component: AboutUsComponent,
+      },
+      {
+        path: 'about-breed',
+        component: AboutBreedComponent,
+      },
+      {
+        path: 'how-to-care',
+        component: HowToCareComponent,
+      },
+    ],
+  },
   { path: 'auth/login', component: LoginComponent },
   {
     path: 'admin',
