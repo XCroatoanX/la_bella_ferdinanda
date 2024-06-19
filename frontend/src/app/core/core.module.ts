4f8cb7320d66f4318b7bf51fadaa8d288297d08f
XCroatoanX <stasyukartem2@gmail.com>
2024-06-19 16:42:38 +0200

    Added core pages

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from "@angular/core";
-import { HeaderComponent } from "./header/header.component";
-import { FooterComponent } from "./footer/footer.component";
+import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NgOptimizedImage } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-    declarations: [HeaderComponent, FooterComponent],
-    imports: [],
-    exports: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent],
+  imports: [NgOptimizedImage, RouterModule],
+  exports: [HeaderComponent, FooterComponent],
 })
-
-export class CoreModule {
-}
+export class CoreModule {}
