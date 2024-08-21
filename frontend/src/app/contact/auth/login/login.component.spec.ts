a30b892c11b99924bb59ccc72ad05ee23e8c8065
XCroatoanX <stasyukartem2@gmail.com>
2024-08-21 15:20:41 +0200

    Added back the auth part

--- frontend/src/app/contact/auth/login/login.component.spec.ts
+++ frontend/src/app/contact/auth/login/login.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AboutFoodComponent } from './about-food.component';
+import { LoginComponent } from './login.component';
 
-describe('AboutFoodComponent', () => {
-  let component: AboutFoodComponent;
-  let fixture: ComponentFixture<AboutFoodComponent>;
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AboutFoodComponent]
+      imports: [LoginComponent]
     })
     .compileComponents();
-
-    fixture = TestBed.createComponent(AboutFoodComponent);
+    
+    fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
