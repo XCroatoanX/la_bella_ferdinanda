dbb757b34e0ab9c1a2684a0aba084313084b840a
XCroatoanX <stasyukartem2@gmail.com>
2024-08-21 15:20:41 +0200

    Added back the auth part

---
+++ frontend/src/app/contact/auth/login/login.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router, RouterModule } from '@angular/router';
+import { AuthResponse } from '../../models/auth-response.model';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, RouterModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  username: FormControl;
+  password: FormControl;
+  alertMessage: string = '';
+  alertType: string = '';
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    this.username = new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/),
+    ]);
+    this.password = new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.maxLength(30),
+      Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/),
+    ]);
+    this.loginForm = this.fb.group({
+      username: this.username,
+      password: this.password,
+    });
+  }
+
+  public onSubmit(): void {
+    this.authService
+      .login(this.loginForm.value)
+      .subscribe((authReponse: AuthResponse) => {
+        console.log('AuthResponse: ', authReponse);
+        this.router.navigate(['/admin']);
+      });
+  }
+}
