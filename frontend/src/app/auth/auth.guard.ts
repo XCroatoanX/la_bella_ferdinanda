ef1cf99a5af3ed2ec745cc0e95c52fdf25ad2bc8
XCroatoanX <stasyukartem2@gmail.com>
2024-08-21 15:31:40 +0200

    Refactoring

--- frontend/src/app/auth/auth.guard.ts
+++ frontend/src/app/auth/auth.guard.ts
@@ -1,13 +1,8 @@
-import { CanActivateFn } from '@angular/router';
-import { TokenService } from '../services/token.service';
-import { inject } from '@angular/core';
+import {CanActivateFn} from '@angular/router';
+import {TokenService} from '../services/token.service';
+import {inject} from '@angular/core';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const tokenService: TokenService = inject(TokenService);
-
-  if (tokenService.isValid()) {
-    return true;
-  }
-
-  return false;
+  return tokenService.isValid();
 };
