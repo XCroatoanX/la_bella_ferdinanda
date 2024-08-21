67b23f198dd7c90f92b00e65d2df43970df88404
XCroatoanX <stasyukartem2@gmail.com>
2024-08-21 16:25:52 +0200

    Reworked Home page

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
-import { CoreModule } from '../core/core.module';
-import { RouterLink } from '@angular/router';
+import {Component} from '@angular/core';
+import {CoreModule} from '../core/core.module';
+import {RouterLink} from '@angular/router';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CoreModule, RouterLink],
+  imports: [CoreModule, RouterLink, NgOptimizedImage],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+}
