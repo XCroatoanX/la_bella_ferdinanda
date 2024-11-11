a4fe65f968d72e7c16c4cd29286a1c00d3af23b6
XCroatoanX <stasyukartem2@gmail.com>
2024-11-11 21:22:53 +0100

    Removed lombok, issues with building

--- backend/src/main/java/com/example/backend/models/Admin.java
+++ backend/src/main/java/com/example/backend/models/Admin.java
@@ -4,24 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.util.UUID;
 
 @Entity(name = "Admin")
-@NoArgsConstructor
 public class Admin {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-  
-    @Setter
-    @Getter
+
     private String username;
-    @Setter
-    @Getter
     private String password;
 
     public Admin(String username, String password) {
@@ -29,4 +20,23 @@ public class Admin {
         this.password = password;
     }
 
+    public Admin() {
+
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
