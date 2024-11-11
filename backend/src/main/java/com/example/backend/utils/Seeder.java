8a6f840071c412cb3b82a16a099835ed22e951ba
XCroatoanX <stasyukartem2@gmail.com>
2024-11-11 21:22:53 +0100

    Removed lombok, issues with building

--- backend/src/main/java/com/example/backend/utils/Seeder.java
+++ backend/src/main/java/com/example/backend/utils/Seeder.java
@@ -1,11 +1,12 @@
 package com.example.backend.utils;
 
-import com.example.backend.dao.AdminRepository;
-import com.example.backend.models.Admin;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import com.example.backend.dao.AdminRepository;
+import com.example.backend.models.Admin;
 
 @Component
 public class Seeder {
@@ -22,8 +23,8 @@ public class Seeder {
 
     public void seedAdmin() {
         Admin admin = new Admin();
-        admin.setUsername("Artemka2004#");
-        admin.setPassword(new BCryptPasswordEncoder().encode("Kyryll2006@"));
+        admin.setUsername("sinyaka@gmail.com");
+        admin.setPassword(new BCryptPasswordEncoder().encode("Labellaferdinanda@2024"));
         adminRepository.save(admin);
 
     }
