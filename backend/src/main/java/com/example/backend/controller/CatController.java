4edf12e7fc9c904a0c501a1d4624edf5124f7be7
XCroatoanX <stasyukartem2@gmail.com>
2024-11-12 21:22:28 +0100

    Fixed update error showing up, instead of success

--- backend/src/main/java/com/example/backend/controller/CatController.java
+++ backend/src/main/java/com/example/backend/controller/CatController.java
@@ -1,22 +1,33 @@
 package com.example.backend.controller;
 
-import com.example.backend.dao.CatDAO;
-import com.example.backend.dto.CatDTO;
-import com.example.backend.models.Cat;
-import jakarta.persistence.EntityNotFoundException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.backend.dao.CatDAO;
+import com.example.backend.dto.CatDTO;
+import com.example.backend.models.Cat;
+
+import jakarta.persistence.EntityNotFoundException;
+
 @RestController
-@CrossOrigin(origins = { "http://localhost:4200", "https://labellaferdinanda.netlify.app", "https://labellaferdinanda.nl", "https://www.labellaferdinanda.nl" })
+@CrossOrigin(origins = { "http://localhost:4200", "https://labellaferdinanda.netlify.app",
+        "https://labellaferdinanda.nl", "https://www.labellaferdinanda.nl" })
 @RequestMapping("/cat")
 public class CatController {
     private final CatDAO catDAO;
@@ -40,33 +51,32 @@ public class CatController {
         return ResponseEntity.ok(this.catDAO.getCatById(id));
     }
 
-    @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    @PostMapping(consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
     public ResponseEntity<?> createCat(@RequestPart("cat") CatDTO catDTO,
-                                       @RequestPart("imagefile") MultipartFile[] file) {
+            @RequestPart("imagefile") MultipartFile[] file) {
         try {
             this.catDAO.createCat(catDTO, file);
             Map<String, String> response = new HashMap<>();
             response.put("message", "Created cat: " + catDTO.name);
             return ResponseEntity.ok(response);
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error creating cat: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error creating cat: " + e.getMessage());
         }
     }
 
-    @PutMapping(value = "/{id}", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    @PutMapping(value = "/{id}", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
     public ResponseEntity<?> updateCat(@PathVariable UUID id, @RequestPart("cat") CatDTO catDTO,
-                                       @RequestPart("imagefile") MultipartFile[] file) {
+            @RequestPart("imagefile") MultipartFile[] file) {
         try {
             this.catDAO.updateCat(catDTO, file, id);
             Map<String, String> response = new HashMap<>();
             response.put("message", "Updated cat: " + catDTO.name);
-            ResponseEntity.ok(response);
+            return ResponseEntity.ok(response);
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error updating cat: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error updating cat: " + e.getMessage());
         }
-
-
-        return ResponseEntity.ok("Updated cat: " + catDTO.name);
     }
 
     @DeleteMapping("/{id}")
