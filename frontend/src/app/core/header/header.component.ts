c7cf3204a7cbbf3fa23f3589c2b60d2459a2bcd8
--global <--global>
2024-11-27 18:43:05 +0100

    Fixed coremodule

--- frontend/src/app/core/header/header.component.ts
+++ frontend/src/app/core/header/header.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
+  standalone: false
 })
 export class HeaderComponent {
 
