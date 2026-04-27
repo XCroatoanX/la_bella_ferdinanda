80a993079ca1fd33ddcba865e6fc2cb2a65d919a
XCroatoanX <stasyukartem2@gmail.com>
2026-04-27 17:30:52 +0200

    Updated to latest version

--- backend/src/main/java/com/example/backend/services/CredentialValidator.java
+++ backend/src/main/java/com/example/backend/services/CredentialValidator.java
@@ -1,22 +1,21 @@
 package com.example.backend.services;
 
 import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import org.passay.CharacterRule;
-import org.passay.EnglishCharacterData;
-import org.passay.LengthRule;
+import org.passay.DefaultPasswordValidator;
 import org.passay.PasswordData;
 import org.passay.PasswordValidator;
-import org.passay.RuleResult;
-import org.passay.WhitespaceRule;
+import org.passay.ValidationResult;
+import org.passay.data.EnglishCharacterData;
+import org.passay.rule.CharacterRule;
+import org.passay.rule.LengthRule;
+import org.passay.rule.WhitespaceRule;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CredentialValidator {
     public boolean isValidPassword(String password) {
-        PasswordValidator validator = new PasswordValidator(Arrays.asList(
+        PasswordValidator validator = new DefaultPasswordValidator(Arrays.asList(
                 new LengthRule(8, 30),
                 new CharacterRule(EnglishCharacterData.Digit, 1),
                 new CharacterRule(EnglishCharacterData.LowerCase, 1),
@@ -24,19 +23,19 @@ public class CredentialValidator {
                 new CharacterRule(EnglishCharacterData.Special, 1),
                 new WhitespaceRule()));
 
-        RuleResult result = validator.validate(new PasswordData(password));
+        ValidationResult result = validator.validate(new PasswordData(password));
         return result.isValid();
     }
 
     public boolean isValidUsername(String username) {
-        PasswordValidator validator = new PasswordValidator(Arrays.asList(
+        PasswordValidator validator = new DefaultPasswordValidator(Arrays.asList(
                 new LengthRule(8, 30),
                 new CharacterRule(EnglishCharacterData.Digit, 1),
                 new CharacterRule(EnglishCharacterData.LowerCase, 1),
                 new CharacterRule(EnglishCharacterData.UpperCase, 1),
                 new CharacterRule(EnglishCharacterData.Special, 1),
                 new WhitespaceRule()));
-        RuleResult result = validator.validate(new PasswordData(username));
+        ValidationResult result = validator.validate(new PasswordData(username));
         return result.isValid();
     }
 }
